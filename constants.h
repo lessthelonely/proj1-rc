b5eb5768a3fbec71f7ef2d88a9e5353bf0c23102
melisilva <55757043+melisilva@users.noreply.github.com>
2021-11-10 17:31:40 +0000

    Added constants + byte stuffing

---
+++ constants.h
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+#define BIT(n) 1 << n
+
+#define FLAG 0x7E
+
+#define A_E 0x03 //Comandos do Emissor e Respostas do Receptor
+#define A_R 0x01 //Comandos do Receptor e Respostas do Emissor
+
+#define C_SET 0x03
+#define C_DISC 0x0B
+#define C_UA 0x07
+#define C_RR_ONE 0x85
+#define C_RR_ZERO 0x05
+#define C_REJ_ONE 0x11
+#define C_REJ_ZERO 0x01
+#define C_I_ONE 0x40
+#define C_I_ZERO 0x00
+
+#define BCC_SET (A_E^C_SET) //SET is always sent by emissor
+#define BCC_UA_E (A_E^C_UA)
+#define BCC_UA_R (A_R^C_UA)
+#define BCC_DISC_E (A_E^C_DISC)
+#define BCC_DISC_R (A_R^C_DISC)
+#define BCC_C_I_ONE (A_E^C_I_ONE)
+#define BCC_C_I_ZERO (A_E^C_I_ZERO)
+#define BCC_RR_ONE (A_E^C_RR_ONE)
+#define BCC_RR_ZERO (A_E^C_RR_ZERO)
+#define BCC_REJ_ONE (A_E^C_REJ_ONE)
+#define BCC_REJ_ZERO (A_E^C_REJ_ZERO)
+
+#define ESC 0x7D
+#define FLAG_FOUND 0x5E    //FLAG ^ 0x20
+#define ESC_FOUND 0x5D  //ESC ^ 0x20
--- writenoncanonical.c
+++ writenoncanonical.c
@@ -16,157 +16,139 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP = FALSE;
+volatile int STOP=FALSE;
 int got_UA = FALSE;
 
-const u_int8_t BCC = A ^ C; //BCC=XOR(A,C)
-int flag = 1, conta = 1;
+const u_int8_t BCC = A ^ C;  //BCC=XOR(A,C)
+int flag =1, conta=1;
 int fd;
 u_int8_t set[5] = {FLAG, A, C, BCC, FLAG};
 
-void send_set()
-{
+void send_set(){
   int res;
-  res = write(fd, set, sizeof(set));
-  if (res == -1)
-  {
-    printf("ERROR\n");
-    exit(1);
-  }
+  res = write(fd,set,sizeof(set));  
+  if(res==-1){
+      printf("ERROR\n");
+      exit(1);
+  } 
 }
 
-void read_ua()
-{
-  int i = 0;
+void read_ua(){
+  int i=0;
   int res;
   u_int8_t buf[255];
 
-  while (STOP == FALSE)
-  { /* loop for input */
+  while (STOP==FALSE) {       /* loop for input */
     u_int8_t byte_received;
-    res = read(fd, &byte_received, 1); /* returns after 5 chars have been input */
-    if (res == -1)
-    {
+    res = read(fd,&byte_received,1);   /* returns after 5 chars have been input */
+    if(res==-1){
       printf("ERROR\n");
       exit(1);
     }
-    if (i == 0 && byte_received != FLAG)
-    {
+    if(i==0 && byte_received !=FLAG){
       continue;
     }
-    if (i > 0 && byte_received == FLAG)
-    { //last byte sent by SET
-      STOP = TRUE;
+    if(i>0 && byte_received == FLAG){ //last byte sent by SET
+      STOP =TRUE;
     }
-    buf[i++] = byte_received; /* so we can printf... */
-  }
+    buf[i++]=byte_received;               /* so we can printf... */
+    }
 
-  got_UA = TRUE;
+    got_UA = TRUE;
 
-  printf("Printing UA:\n");
-  for (int j = 0; j < 5; j++)
-  {
-    printf("%u\n", buf[j]);
-  }
+    printf("Printing UA:\n");
+    for(int j=0;j<5;j++){
+      printf("%u\n",buf[j]);
+    }
 }
 
 void atende() // atende alarme
 {
-  /*u_int8_t set[5]={FLAG,A,C,BCC,FLAG};
-
-  write(fd,set,sizeof(set)); //send SET again
-	conta++;*/
-
-  if (!got_UA)
-  {
+   if(!got_UA){
     send_set();
-    
     printf("alarme # %d\n", conta);
     conta++;
-    if (conta < 4)
-    {
+    if(conta < 4){
       alarm(3);
     }
-    else
-    {
+    else{
       printf("ERROR - Timeout over. It wasn't possible to receive UA successfully\n");
       exit(1);
     }
   }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  int c;
-  struct termios oldtio, newtio;
-  int i, sum = 0, speed = 0;
+    int c;
+    struct termios oldtio,newtio;
+    int i, sum = 0, speed = 0;
+    
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS10", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
 
-  if ((argc < 2) ||
-      ((strcmp("/dev/ttyS10", argv[1]) != 0) &&
-       (strcmp("/dev/ttyS11", argv[1]) != 0)))
-  {
-    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-    exit(1);
-  }
 
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
 
-  fd = open(argv[1], O_RDWR | O_NOCTTY);
-  if (fd < 0)
-  {
-    perror(argv[1]);
-    exit(-1);
-  }
 
-  if (tcgetattr(fd, &oldtio) == -1)
-  { /* save current port settings */
-    perror("tcgetattr");
-    exit(-1);
-  }
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
 
-  bzero(&newtio, sizeof(newtio));
-  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-  newtio.c_iflag = IGNPAR;
-  newtio.c_oflag = 0;
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
 
-  /* set input mode (non-canonical, no echo,...) */
-  newtio.c_lflag = 0;
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
 
-  newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
-  newtio.c_cc[VMIN] = 5;  /* blocking read until 5 chars received */
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
-  tcflush(fd, TCIOFLUSH);
 
-  if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-  {
-    perror("tcsetattr");
-    exit(-1);
-  }
 
-  printf("New termios structure set\n");
+    tcflush(fd, TCIOFLUSH);
 
-  signal(SIGALRM, atende); //instala rotina que atende interrupção
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
 
-  send_set(); //Send SET
+    printf("New termios structure set\n");
 
-  alarm(3);
+    signal(SIGALRM, atende);  //instala rotina que atende interrupção
 
-  read_ua(); //Receive UA
-
-  sleep(1);
-  if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
-  {
-    perror("tcsetattr");
-    exit(-1);
-  }
-  close(fd);
-  return 0;
+   
+    send_set();//Send SET
+    
+    alarm(3);
+    
+    read_ua();//Receive UA
+    
+    sleep(1);
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+    close(fd);
+    return 0;
 }
