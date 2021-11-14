6ec32edf3887262c18448bc9e50ed02d3d3e54c1
melisilva <55757043+melisilva@users.noreply.github.com>
2021-11-14 19:26:28 +0000

    Created data package

--- app.c
+++ app.c
@@ -17,11 +17,24 @@ Contents of each packet->slide 23
 Packages are sent by TRANSMITTER-->need to make functions to read them? Yeah, probs
 */
 
+/* Return -1 if error, 0 otherwise
+*/
 int create_data_package(int length, char*data,char*package){
+    //ASK TEACHER: check how N, L2 and L1 are calculated
+    package[0] = 1; //C – campo de controlo (valor: 1 – dados)
+    package[1] = link_info.sequenceNumber; //%255->does it only want the sequenceNumber or is it, sequenceNumber % 255
+    package[2] = length / 256; //L2
+    package[3] = length % 256; //L1
+    if(memcpy(&package[4],data,length) == NULL){ //memcpy returns destination pointer
+       printf("ERROR\n");
+       return -1;
+    }
+    printf("Data package was created");
+    return 0;
 }
 
 int create_control_package(int c,char* file_name, int length, char*package){
-    
+
 }
 
 int read_data_package(){}
