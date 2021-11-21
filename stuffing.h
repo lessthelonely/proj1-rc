060b64589ec3c470302b0c7758d3becddd6a768a
melisilva <melissa.silva2001.ms@gmail.com>
2021-11-21 18:47:05 +0000

    Deleted extra files

--- stuffing.h
+++
@@ -1,9 +0,0 @@
-#ifndef STUFFING_H
-#define STUFFING_H
-
-//We should use char** because it's a pointer to char*
-
-int stuffing(u_int8_t* buffer, int length);
-int destuffing(u_int8_t* buffer, int length);
-
-#endif
