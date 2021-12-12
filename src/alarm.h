e3b7a0691cba928ef3fe9261e7e419dacffd9745
melisilva <55757043+melisilva@users.noreply.github.com>
2021-12-12 22:40:17 +0000

    Added docummentation to alarm.h

--- src/alarm.h
+++ src/alarm.h
@@ -1,8 +1,19 @@
 #ifndef ALARM_H
 #define ALARM_H
 
+/**
+     * Alarm handler: increases a counter of the number of tries of a certain action and checks if it has exceeded the number of tries permitted
+*/
 void atende();
+
+/**
+     * Installs alarm 
+*/
 void install_alarm();
+
+/**
+     * Deactivates the alarm
+*/
 void deactivate_alarm();
 
 #endif
