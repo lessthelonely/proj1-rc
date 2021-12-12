f621ea8db7e0c7c68e9869b69ee4aafc0ff01abf
melisilva <55757043+melisilva@users.noreply.github.com>
2021-12-12 22:48:09 +0000

    Added docummentation to header file stuffing

--- src/stuffing.h
+++ src/stuffing.h
@@ -1,7 +1,20 @@
 #ifndef STUFFING_H
 #define STUFFING_H
 
+/**
+     * Function that executes the stuffing process in a frame, adding new bytes to it depending on the bytes found in it. Called before trama is sent to receiver.
+     * @param buffer: frame.
+     * @param length: size of frame buffer.
+     * @return: size of the new frame.
+*/
 int stuffing(u_int8_t* buffer, int length);
+
+/**
+    * Function that executes the destuffing process in a frame, removing specific bytes from it. Called by the receiver in order to store information.
+     * @param buffer: frame.
+     * @param length: size of frame buffer.
+     * @return: size of the new frame.
+*/
 int destuffing(u_int8_t* buffer, int length);
 
 #endif
