53d0f6cb9a5c060c0c6ac63d38a7e9f658a372f1
melisilva <55757043+melisilva@users.noreply.github.com>
2021-12-13 10:05:26 +0000

    Update data_protocol.h

--- src/data_protocol.h
+++ src/data_protocol.h
@@ -17,22 +17,25 @@ typedef enum
      * @param command: int from 0 to 6, to specificy which trama is to be sent (SET, DISC, UA, RR with sequence number one, 
      RR with sequence number zero, REJ with sequence number one or REJ with sequence number zero).
      * @param sender: indicates who wishes to send the trama, TRANSMITTER or RECEIVER.
+     * @param fd: file Descriptor of the serial port where to write the trama.
      * @return: number of bytes written (return value of function write) or -1 in case of error.
 */
-int send_cmd(int command, int sender);
+int send_cmd(int command, int sender, int fd);
 
 /**
     * Function that reads the information trama and stores the information portion of it to an array.
      * @param info_trama: array where information portion of information trama is stored.
      * @param cmd: variable where C (campo de controlo) is stored.
+     * @param fd: file Descriptor of the serial port where to read trama from.
      * @return: size of info_trama array or -1 in case of error.
 */
-int read_info_trama(u_int8_t* info_trama,u_int8_t*cmd);
+int read_info_trama(u_int8_t* info_trama,u_int8_t*cmd, int fd);
 
 /**
     * Function that reads supervision and non numbered tramas (SET, DISC, UA, RR, REJ).
      * @param cmd: variable where C (campo de controlo) is stored.
+     * @param fd: file Descriptor of the serial port where to read trama from.
      * @return: 0 or -1 in case of error.
 */
-int read_cmd(u_int8_t* cmd);
+int read_cmd(u_int8_t* cmd, int fd);
 #endif
