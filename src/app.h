55483eb268d1f993cfce7cbca74b78b4392e14ec
melisilva <55757043+melisilva@users.noreply.github.com>
2021-12-13 09:50:57 +0000

    Update app.h

--- src/app.h
+++ src/app.h
@@ -1,23 +1,11 @@
 #ifndef APP_H
 #define APP_H
 
-struct linkLayer
-{
-    char port[20];                 /*Dispositivo /dev/ttySx, x = 0, 1*/
-    int baudRate;                  /*Velocidade de transmissão*/
-    unsigned int sequenceNumber;   /*Número de sequência da trama: 0, 1*/
-    unsigned int timeout;          /*Valor do temporizador: 1 s*/
-    unsigned int numTransmissions; /*Número de tentativas em caso de falha*/
-};
-
 struct applicationLayer
 {
-    int fileDescriptor; /*Descritor correspondente à porta série*/
-    int status;         /*TRANSMITTER | RECEIVER*/
     int sequenceNumber;
 };
 
-struct linkLayer link_info;
 struct applicationLayer app_info;
 
 /**
