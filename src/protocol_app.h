1fe0faa4d0c649f26960cb11570f540f088fead7
melisilva <55757043+melisilva@users.noreply.github.com>
2021-12-13 09:50:53 +0000

    Update protocol_app.h

--- src/protocol_app.h
+++ src/protocol_app.h
@@ -1,6 +1,19 @@
 #ifndef PROTOCOL_APP_H
 #define PROTOCOL_APP_H
 
+struct linkLayer
+{
+    char port[20];                 /*Dispositivo /dev/ttySx, x = 0, 1*/
+    int baudRate;                  /*Velocidade de transmissão*/
+    unsigned int sequenceNumber;   /*Número de sequência da trama: 0, 1*/
+    unsigned int timeout;          /*Valor do temporizador: 1 s*/
+    unsigned int numTransmissions; /*Número de tentativas em caso de falha*/
+    int fileDescriptor;
+    int status;
+};
+
+struct linkLayer link_info;
+
 /**
     * Function that opens serial ports and connects transmitter and receiver.
      * @return: 0 or -1 in case of error.
